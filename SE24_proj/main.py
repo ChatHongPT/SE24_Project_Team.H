5f1a6e3a892559990b51f4148438a632648f1174
jesuszero <djawndud5923@naver.com>
2024-11-10 17:32:09 +0900

    acc

--- SE24_proj/main.py
+++ SE24_proj/main.py
@@ -21,7 +21,7 @@ def execute_command_callback(command, car_controller):
                 car_controller.lock_left_door()
         else:
             car_controller.close_left_door()
-            car_controller.lock_left_door()git
+            car_controller.lock_left_door()
 
         # 오른쪽 문 검증
         if car_controller.get_right_door_status() == "Closed":
