f77df268a37c3c6750cb196aaa8b0d96dbde0cef
soons <soonso00@naver.com>
2024-11-14 16:45:48 +0900

    commit for merge

--- SE24_proj/main.py
+++ SE24_proj/main.py
@@ -15,21 +15,26 @@ def execute_command_callback(command, car_controller):
             car_controller.toggle_engine() # 시동 ON / OFF
 
     elif command == "ACCELERATE":
+        if not car_controller.get_engine_status():
+            print("Command failed: 시동이 꺼져있어 가속할 수 없습니다.")
+            return
         if car_controller.get_speed() >= 200:
             print("200 이상으로 속도를 높힐 수 없음")
             return
-        if not car_controller.get_trunk_status() :
+        if not car_controller.get_trunk_status():
             print("트렁크 문이 열려있습니다. 가속할 수 없습니다.")
             return
-        if car_controller.get_right_door_status() == "Opened":
+        if car_controller.get_right_door_status() == "OPEN":
             print("오른쪽 문이 열려있습니다. 가속할 수 없습니다.")
             return
-        if car_controller.get_left_door_status() == "Opened":
+        if car_controller.get_left_door_status() == "OPEN":
             print("왼쪽 문이 열려있습니다. 가속할 수 없습니다.")
             return
         car_controller.accelerate()  # 속도 +10
         if car_controller.get_speed() >= 10:
             car_controller.lock_vehicle()
+            car_controller.lock_left_door()  # 각 도어도 잠가야 함
+            car_controller.lock_right_door()
 
     elif command == "BRAKE":
          car_controller.brake()   # 감속 -10
@@ -41,7 +46,7 @@ def execute_command_callback(command, car_controller):
             print("Command failed: 오른쪽 문이 열려있습니다.")
         elif car_controller.get_left_door_status() == "OPEN":
             print("Command failed: 왼쪽 문이 열려있습니다.")
-        elif not car_controller.get_trunk_status:
+        elif not car_controller.get_trunk_status():
             print("Commad failed: 트렁크가 열려있습니다.")
         else:
             car_controller.lock_vehicle()
@@ -87,20 +92,20 @@ def execute_command_callback(command, car_controller):
         car_controller.unlock_right_door()  # 오른쪽문 잠금해제
 
     elif command == "LEFT_DOOR_OPEN":
-        if car_controller.get_left_door_status():  # 문이 이미 열려 있는지 확인
+        if car_controller.get_left_door_status() == "OPEN":  # 문이 이미 열려 있는지 확인
             print("Command failed: 왼쪽 문이 이미 열려 있습니다.")
             return
-        if car_controller.get_left_door_lock():  # 문이 잠겨 있는지 확인
+        if car_controller.get_left_door_lock() == "LOCKED":  # 문이 잠겨 있는지 확인
             print("Command failed: 왼쪽 문이 잠겨 있어 열 수 없습니다.")
             return
         car_controller.open_left_door()  # 문 열기
         print("왼쪽 문이 열렸습니다.")
 
     elif command == "RIGHT_DOOR_OPEN":
-        if car_controller.get_right_door_status():  # 문이 이미 열려 있는지 확인
+        if car_controller.get_right_door_status() == "OPEN":  # 문이 이미 열려 있는지 확인
             print("Command failed: 오른쪽 문이 이미 열려 있습니다.")
             return
-        if car_controller.get_right_door_lock():  # 문이 잠겨 있는지 확인
+        if car_controller.get_right_door_lock() == "LOCKED":  # 문이 잠겨 있는지 확인
             print("Command failed: 오른쪽 문이 잠겨 있어 열 수 없습니다.")
             return
         car_controller.open_right_door()  # 문 열기
@@ -108,14 +113,14 @@ def execute_command_callback(command, car_controller):
 
     # 문 닫기
     elif command == "LEFT_DOOR_CLOSE":
-        if not car_controller.get_left_door_status():  # 문이 열려 있는 상태인지 확인
+        if car_controller.get_left_door_status() == "CLOSED":  # 문이 열려 있는 상태인지 확인
             print("Command failed: 왼쪽 문이 이미 닫혀 있습니다.")
             return
         car_controller.close_left_door()  # 문 닫기
         print("왼쪽 문이 닫혔습니다.")
 
     elif command == "RIGHT_DOOR_CLOSE":
-        if not car_controller.get_right_door_status():  # 문이 열려 있는 상태인지 확인
+        if car_controller.get_right_door_status() == "CLOSED":  # 문이 열려 있는 상태인지 확인
             print("Command failed: 오른쪽 문이 이미 닫혀 있습니다.")
             return
         car_controller.close_right_door()  # 문 닫기
