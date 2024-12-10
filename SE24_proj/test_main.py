b4a363e7b64be51f259e9f8d610a095002e82342
LeeJinGyu <zmfltmvl@hotmail.co.kr>
2024-12-10 18:01:47 +0900

    fix : test_engine_toggle_fail/fail2 주석 변경 및 self.car_controller 수정

--- SE24_proj/test_main.py
+++ SE24_proj/test_main.py
@@ -91,24 +91,24 @@ class TestCarSystem(unittest.TestCase):
         self.assertTrue(self.car_controller.get_trunk_status())  # 트렁크가 닫혔는지 확인
 
     def test_engine_toggle_fail(self):
-        # 트렁크 열기/닫기 테스트
+        # 엔진 버튼만 눌렀을 때 실패
         execute_command_callback("UNLOCK", self.car_controller)
 
         execute_command_callback("ENGINE_BTN", self.car_controller)
-        self.assertFalse(self.car_controller.get_trunk_status())  # 엔진 버튼만 입력 들어올 때 확인
+        self.assertFalse(self.car_controller.get_engine_status())  # 엔진 버튼만 입력 들어올 때 확인
 
         execute_command_callback("ACCELERATE", self.car_controller)
-        self.assertTrue(self.car_controller.get_trunk_status())  # 시동 안 걸렸을 때 가속 가능한지 확인
+        self.assertEqual(self.car_controller.get_speed(), 10)  # 시동 안 걸렸을 때 가속 되었는지 확인
     
     def test_engine_toggle_fail2(self):
-        # 트렁크 열기/닫기 테스트
+        # 브레이크 밟은 상태에서 엔진 버튼 여러 번 눌렀을 때 실패
         execute_command_callback("UNLOCK", self.car_controller)
 
-        execute_command_callback("BRAKE ENGINE_BTN BRAKE ENGINE_BTN BRAKE ENGINE_BTN", self.car_controller)
-        self.assertFalse(self.car_controller.get_trunk_status())  # 한 줄에 여러 인자 값이 들어올 때 예외처리 잘 되는지 확인
+        execute_command_callback("BRAKE ENGINE_BTN ENGINE_BTN ENGINE_BTN ENGINE_BTN ENGINE_BTN", self.car_controller)
+        self.assertFalse(self.car_controller.get_engine_status())  # 한 줄에 여러 인자 값이 들어올 때 예외처리 잘 되는지 확인
 
         execute_command_callback("ACCELERATE", self.car_controller)
-        self.assertTrue(self.car_controller.get_trunk_status())  # 시동 안 걸렸을 때 가속 가능한지 확인
+        self.assertEqual(self.car_controller.get_speed(), 10)  # 시동 안 걸렸을 때 가속 되었는지 확인
 
     def test_sos(self):
         # 긴급 상황 테스트
